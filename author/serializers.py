795af7d4d8375c719c067f31256a0c76cf625b7d
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-05-26 14:45:32 +0300

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    pseudonym = serializers.CharField(required=False)
+    age = serializers.IntegerField()
+    retired = serializers.BooleanField()
+
+    class Meta:
+        model = Author
+        fields = [
+            "first_name", "last_name", "pseudonym", "age", "retired"
+        ]
